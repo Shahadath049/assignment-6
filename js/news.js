582a9df16d95bb4e35059513b0f9305b7e288390
Shahadath049 <zamirshahadath049@gamail.com>
2022-09-03 14:21:07 +0600

    all news added successfully

--- js/news.js
+++ js/news.js
@@ -10,38 +10,42 @@ const displayCategory = categories =>{
         const newDiv = document.createElement('div');
         newDiv.classList.add('news-type')
         newDiv.innerHTML =`
-            <p onclick="loadNews()">${category.category_name}<p>
+            <p onclick="loadNews('${category.category_id}')">${category.category_name}<p>
         `
         categoryContainer.appendChild(newDiv);
     });
     
 }
-
-const loadNews = () =>{
-    fetch('https://openapi.programming-hero.com/api/news/category/01')
+// load news
+const loadNews = (category_id) =>{
+    fetch(`https://openapi.programming-hero.com/api/news/category/${category_id}`)
     .then(res =>res.json())
-    .then(data => console.log(data.data))
+    .then(data => displayNews(data.data))
 }
 
 const displayNews = allnews =>{
     const newsContainer = document.getElementById('news-container');
+    newsContainer.textContent= '';
     allnews.forEach(news =>{
-        newsContainer.innerHTML =`
+        const newsData = document.createElement('div');
+        newsData.innerHTML =`
         <div class="card mb-3" >
         <div class="row g-0">
           <div class="col-md-4">
-            <img src="..." class="img-fluid rounded-start" alt="...">
+            <img src="${news.image_url}" class="img-fluid rounded-start" alt="...">
           </div>
           <div class="col-md-8">
             <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+              <h5 class="card-title">${news.title}</h5>
+              <p class="card-text">${news.details.slice(0,120)}</p>
+              <p class="card-text"><small class="text-muted">${news.author.name}</small></p>
+            
             </div>
           </div>
         </div>
       </div>
         `
+        newsContainer.appendChild(newsData);
     })
     
 }
