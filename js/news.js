b0dccbb99ebafb00b0c58c0624d424a794d77045
Shahadath049 <zamirshahadath049@gamail.com>
2022-09-04 01:56:54 +0600

    modal data Updated

--- js/news.js
+++ js/news.js
@@ -5,14 +5,14 @@ const loadCategory = () =>{
 }
 
 const displayCategory = categories =>{
-    const categoryContainer = document.getElementById('category-container');
+    const categoryContainer = document.getElementById('category-containerr');
     categories.forEach(category => {
-        const newDiv = document.createElement('div');
-        newDiv.classList.add('news-type')
-        newDiv.innerHTML =`
-            <p onclick="loadNews('${category.category_id}')">${category.category_name}<p>
+        const newlist = document.createElement('li');
+        newlist.classList.add('mx-auto')
+        newlist.innerHTML =`
+        <p onclick="loadNews('${category.category_id}')">${category.category_name}<p>
         `
-        categoryContainer.appendChild(newDiv);
+        categoryContainer.appendChild(newlist);
     });
     
 }
@@ -23,13 +23,16 @@ const loadNews = (category_id) =>{
     .then(data => displayNews(data.data))
 }
 
-const displayNews = allnews =>{
+const displayNews = allnews =>{ 
+    
     const newsContainer = document.getElementById('news-container');
     newsContainer.textContent= '';
+   
     allnews.forEach(news =>{
+      
         const newsData = document.createElement('div');
         newsData.innerHTML =`
-        <div class="card mb-3" >
+        <div class="card mb-3" data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="newsDetails('${news._id}')" >
         <div class="row g-0">
           <div class="col-md-4">
             <img src="${news.image_url}" class="img-fluid rounded-start" alt="...">
@@ -38,7 +41,7 @@ const displayNews = allnews =>{
             <div class="card-body">
               <h5 class="card-title">${news.title}</h5>
               <p class="card-text">${news.details.slice(0,120)}</p>
-              <p class="card-text"><small class="text-muted">${news.author.name}</small></p>
+              <p class="card-text"><small class="text-muted">${news.author.name? news.author.name:'No Data'}</small></p>
             
             </div>
           </div>
@@ -49,9 +52,25 @@ const displayNews = allnews =>{
     })
     
 }
+// modal functions
+const newsDetails = news_id =>{
+    fetch(`https://openapi.programming-hero.com/api/news/${news_id}`)
+    .then(res => res.json())
+    .then(data => displayNewsDetails(data.data[0]))
+}
 
-
-
+const displayNewsDetails =  allDetails =>{
+  console.log(allDetails);
+  let modalTitle = document.getElementById('modal-title');
+      modalTitle.innerText = allDetails.title;
+  let modalText = document.getElementById('modal-text');
+      modalText.innerText= allDetails.details;
+  let modalAuthor = document.getElementById('modal-author');
+      modalAuthor.innerText= allDetails.author.name? allDetails.author.name:'No Data found';
+     
+      
+}
 
 
 loadCategory()
+
