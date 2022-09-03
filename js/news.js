a89f424bb84d99f642e62ee632aeb8e3cea03aae
Shahadath049 <zamirshahadath049@gamail.com>
2022-09-03 12:12:20 +0600

    navigation bar completed

---
+++ js/news.js
@@ -0,0 +1,6 @@
+const loadCategory = () =>{
+    fetch('https://openapi.programming-hero.com/api/news/categories')
+    .then(res => res.json())
+    .then(data => console.log(data.data.news_category))
+}
+loadCategory()
